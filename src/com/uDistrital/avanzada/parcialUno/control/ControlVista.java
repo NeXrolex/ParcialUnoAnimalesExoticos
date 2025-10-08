ca591b24ed1bc295b38b6d8be03356e99de54bec
santi <santi@DESKTOP-LU7UNG7>
2025-10-08 18:16:35 -0500

    Clases principales

---
+++ src/com/uDistrital/avanzada/parcialUno/control/ControlVista.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uDistrital.avanzada.parcialUno.control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Alex
+ */
+public class ControlVista implements ActionListener {
+    
+    private ControlGeneral cGeneral;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
