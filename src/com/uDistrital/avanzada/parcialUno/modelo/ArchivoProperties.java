bb98eaf477a3b770926e61020163b2c295db867a
Jeison Rivera <jeisor2005@gmail.com>
2025-10-09 21:08:37 -0500

    Archivos propertieies y raf
    Se agrega lo de los archivos properties y raf para modelo

--- src/com/uDistrital/avanzada/parcialUno/modelo/ArchivoProperties.java
+++ src/com/uDistrital/avanzada/parcialUno/modelo/ArchivoProperties.java
@@ -4,10 +4,46 @@
  */
 package com.uDistrital.avanzada.parcialUno.modelo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  *
- * @author jeiso
+ * @author jeison, Alex
  */
 public class ArchivoProperties {
+
+    //
+    private final File archivo;
     
+    /**
+     *Constructor que asigna la ruta por defecto del archivo
+     * 
+     * @param ruta Ruta del archivo
+     */
+    public ArchivoProperties(String ruta) {
+        this.archivo = new File(ruta);
+    }
+
+    /**
+     * Punto de acceso al archivo para lectura.
+     *
+     * @return Un flujo de entrada para leer el contenido del archivo.
+     * @throws IOException Si ocurre un error al intentar abrir el archivo.
+     */
+    public InputStream open() throws IOException {
+        return new FileInputStream(archivo);
+    }
+    
+    /**
+     * Obtiene el onjeto del archivo
+     * 
+     * @return Archivo
+     */
+    public File getArchivo() {
+        return archivo;
+    }
+
 }
